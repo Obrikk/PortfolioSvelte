2c9349258b172e3cb6486a91bb3e466701f975f8
Gegetropfre <hete.audric@gmail.com>
2024-06-27 08:31:06 +0200

    first commit

---
+++ src/lib/Sites.svelte
@@ -0,0 +1,14 @@
+
+
+
+<div class="sites-wrapper">
+    sites
+</div>
+
+<style>
+    .sites-wrapper{
+        background-color: beige;
+        min-height: 50vh;
+        width: 800px;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Header from '../lib/Header.svelte'
+    import '../styles/global.css'
+</script>
+
+<Header></Header>
+<slot/>
+
